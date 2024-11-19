484c94bc56fa82f2168f325c83eabf8ce0f4500c
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-11-19 14:57:12 +0300

    fix: update button border styles in Banner component for improved visibility

--- src/app/(home)/_components/banner.tsx
+++ src/app/(home)/_components/banner.tsx
@@ -10,8 +10,8 @@ export default function Banner() {
                 <h1 className="lg:text-[58px] sm:text-5xl text-[38px] font-bold">Personalized Nutrition Coaching</h1>
                 <p className="font-medium text-grey-15">Welcome to Nutritionist, your partner in achieving optimal health through personalized nutrition coaching. Our certified nutritionists are here to guide you on your weight loss journey, providing customized plans and ongoing support. Start your transformation today and experience the power of personalized nutrition coaching.</p>
                 <div className="flex items-center justify-center mt-12 gap-x-4 sm:justify-normal">
-                    <Button className="transition-colors duration-300 border border-transparent bg-light-green-70 text-dark-green-15 hover:border-light-green-70 hover:bg-transparent">Get Start Today</Button>
-                    <Button className="transition-colors duration-300 border border-transparent bg-light-green-85 text-dark-green-15 hover:border-light-green-85 hover:bg-transparent">Book Demo</Button>
+                    <Button className="transition-colors duration-300 border-2 border-transparent bg-light-green-70 text-dark-green-15 hover:border-light-green-70 hover:bg-transparent">Get Start Today</Button>
+                    <Button className="transition-colors duration-300 border-2 border-transparent bg-light-green-85 text-dark-green-15 hover:border-light-green-85 hover:bg-transparent">Book Demo</Button>
                 </div>
             </article>
         </section>
