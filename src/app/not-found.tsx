e50e363d1d00f516e1694a81a83d06030622131a
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-11-28 14:48:41 +0300

    feat: update NotFound component styles; change colors for improved visibility and user experience

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,13 +5,13 @@ import Link from "next/link"
 export default function NotFound() {
     return (
         <div className="flex items-center justify-between w-screen ">
-            <div className="flex flex-col items-center justify-between px-5 text-gray-700 md:flex-row">
+            <div className="flex flex-col items-center justify-between px-5 text-grey-15 md:flex-row">
                 <div className="w-full mx-8 lg:w-1/2 dark:text-slate-100">
-                    <span className="mb-8 font-bold text-blue-500 text-7xl">404</span>
+                    <span className="mb-8 font-bold text-light-green-70 text-7xl">404</span>
                     <p className="mb-8 text-2xl font-light leading-normal md:text-3xl">
                         Sorry we couldn&apos;t find this page.
                     </p>
-                    <Link href="/" className="inline px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-500 bg-blue-600 border border-transparent rounded-lg shadow-2xl focus:outline-none active:bg-purple-600 hover:bg-purple-700">
+                    <Link href="/" className="inline px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-500 bg-dark-green-15 border border-transparent rounded-lg shadow-2xl focus:outline-none active:light-dark-green-25 hover:bg-dark-green-30">
                         Go back home
                     </Link>
                 </div>
