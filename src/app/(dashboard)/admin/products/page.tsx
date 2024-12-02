4e72524e5df451a51627f1def52e4d76aa12921d
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-12-02 16:03:50 +0300

    fix: update link path for new product creation in admin products page

--- src/app/(dashboard)/admin/products/page.tsx
+++ src/app/(dashboard)/admin/products/page.tsx
@@ -17,7 +17,7 @@ export default async function page({ searchParams }: Props) {
         <div className="flex flex-col gap-10">
             <div className="flex items-center justify-between my-5">
                 <Search placeholder="Search products" option1="title" option2="category" />
-                <Link href="admins/products/new-product" className="inline px-5 py-3 leading-5 text-white transition-colors duration-300 bg-light-green-70 dark:bg-dark-green-15 border border-transparent rounded-lg shadow-2xl focus:outline-none active:bg-light-green-85 hover:bg-light-green-80 dark:active:bg-dark-green-25 dark:hover:bg-dark-green-25">
+                <Link href="/admin/products/new-product" className="inline px-5 py-3 leading-5 text-white transition-colors duration-300 bg-light-green-70 dark:bg-dark-green-15 border border-transparent rounded-lg shadow-2xl focus:outline-none active:bg-light-green-85 hover:bg-light-green-80 dark:active:bg-dark-green-25 dark:hover:bg-dark-green-25">
                     Add new product
                 </Link>
             </div>
