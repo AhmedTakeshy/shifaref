e338588fd23fd6eba7db3549f1e93432e0aaa3ee
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-12-02 16:03:44 +0300

    feat: add new product creation page with ProductForm component

---
+++ src/app/(dashboard)/admin/products/new-product/page.tsx
@@ -0,0 +1,12 @@
+import ProductForm from "../_components/productForm";
+
+
+export default function page() {
+
+    return (
+        <div className='flex flex-col items-center justify-center w-full max-w-xl dark:text-slate-800 gap-8 mx-auto mt-12'>
+            <h1 className='text-4xl font-semibold'>Create a new product.</h1>
+            <ProductForm />
+        </div>
+    )
+}
