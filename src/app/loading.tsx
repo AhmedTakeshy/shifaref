4e1073f30b37ae4a08f8768a6a6ed74ed31eda24
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-12-02 11:23:56 +0300

    feat: update loading component background color to dark green for improved visibility

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Loading() {
     return (
-        <div className="flex items-center justify-center w-screen h-screen bg-black">
+        <div className="flex items-center justify-center w-screen h-screen bg-dark-green-15">
             <Image src="/favicons/android-chrome-512x512.png" alt="loading" width={100} height={100} className="w-[100px] animate-pulse" />
         </div>
     )
