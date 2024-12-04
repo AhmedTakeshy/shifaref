d6b92c07c654ac00ccd56ef1f0daf66ea384bdc1
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-12-04 14:41:23 +0300

    refactor: update Product and Category models; replace category with categoryName and add checkoutUrl

--- prisma/migrations/20241204093300_init/migration.sql
+++ prisma/migrations/20241204093300_init/migration.sql
@@ -1,6 +1,9 @@
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('SUPER_ADMIN', 'ADMIN');
 
+-- CreateEnum
+CREATE TYPE "CategoryTpe" AS ENUM ('DEFAULT', 'CUSTOM');
+
 -- CreateTable
 CREATE TABLE "Contact" (
     "id" SERIAL NOT NULL,
@@ -33,14 +36,26 @@ CREATE TABLE "User" (
 CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
+    "categoryName" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "images" TEXT[],
+    "checkoutUrl" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "CategoryTpe" NOT NULL DEFAULT 'DEFAULT',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -59,6 +74,12 @@ CREATE TABLE "Blog" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryName_fkey" FOREIGN KEY ("categoryName") REFERENCES "Category"("name") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Blog" ADD CONSTRAINT "Blog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
