6aee705a3f54b3c7c87d4618eb839f77b8f4adb6
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2024-12-05 16:12:01 +0300

    fix: update searchParams handling in messages page to support async retrieval and improve pagination logic

--- src/app/(dashboard)/admin/messages/page.tsx
+++ src/app/(dashboard)/admin/messages/page.tsx
@@ -4,14 +4,16 @@ import PaginationControl from '@/_components/paginationControl'
 import Search from '../_components/search'
 
 type Props = {
-    searchParams: { [key: string]: string | undefined }
+    searchParams: Promise<{ [key: string]: string | undefined }>
 }
 
 export default async function page({ searchParams }: Props) {
+    const { page, search, read } = await searchParams
+    const searchObj = search ? JSON.parse(search) : {}
     const response = await getMessages({
-        page: searchParams?.page,
-        search: { email: searchParams?.email, phone: searchParams?.phone },
-        read: searchParams?.read === "true",
+        page: page || "1",
+        search: { email: searchObj.email, phone: searchObj.phone },
+        read: read === "true",
     })
     return (
         <div className={`dark:bg-slate-800 bg-slate-200 p-5 rounded-xl mt-5 flex flex-col`}>
@@ -19,7 +21,7 @@ export default async function page({ searchParams }: Props) {
             {response.status === "Success" ?
                 <>
                     <MessageCard messages={response.data.messages} />
-                    <PaginationControl metadata={response.data.metadata} currentPage={Number(searchParams.page) || 1} />
+                    <PaginationControl metadata={response.data.metadata} currentPage={Number(page) || 1} />
                 </>
                 :
                 <p className="font-semibold text-center text-red-500">There is a problem with our servers at moment, please try again later.</p>
