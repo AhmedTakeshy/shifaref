7aec381f0b9ffc6bf331fa28d2793b7d734deb22
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-01-25 16:10:11 +0300

    feat: enhance cookie consent component with image and improved layout; update robots.txt for sitemap and host entries

--- src/app/(home)/_components/cookies.tsx
+++ src/app/(home)/_components/cookies.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react';
 import { useGetCookie, useSetCookie } from 'cookies-next/client';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import Image from 'next/image';
+import cookiesImg from '@/../public/imgs/cookies.png';
 
 type CookiesProps = {
     cookie: string;
@@ -27,30 +29,33 @@ export default function Cookies({ cookie }: CookiesProps) {
 
     if (cookieState === 'not-answered') {
         return (
-            <div className="fixed sm:bottom-6 bottom-0 sm:left-10 left-0 w-full sm:max-w-sm rounded-lg border p-6 shadow-lg z-[51] bg-light-green-90">
-                <h2 className="text-xl font-semibold text-slate-700">🍪 Use of cookies</h2>
-                <p className="mb-4 mt-2 text-slate-600">
-                    We use our own and third-party cookies to improve your experience and our services by analyzing how you use our website. For more information, please read our{' '}
-                    <Link
-                        href="/policy"
-                        className="cursor-pointer underline hover:text-blue-500"
-                        target='_blank'
-                        rel={"noopener noreferrer"}
-                        aria-label="More info"
-                        aria-description='More info about cookies'
-                    >
-                        Cookies Policy
-                    </Link>
-                </p>
-                <div className="text-right">
-                    <button
-                        onClick={() => {
-                            handleConsent('accepted');
-                        }}
-                        className="text-md inline-block rounded-lg px-4 py-1 text-right font-semibold text-dark-green-15 transition duration-500 hover:bg-dark-green-40"
-                    >
-                        Accept
-                    </button>
+            <div className="fixed sm:bottom-6 bottom-0 sm:left-10 left-0 sm:max-w-[50rem] w-full rounded-lg border p-6 shadow-lg z-[51] bg-light-green-90">
+                <div className="grid grid-cols-[auto,1fr] gap-4">
+                    <Image src={cookiesImg} alt="Cookies" width={100} height={100} className='rounded' />
+                    <div className="flex flex-col">
+                        <h2 className="text-xl font-semibold text-slate-700">Use of cookies</h2>
+                        <p className=" mt-2 text-slate-600">
+                            We use our own and third-party cookies to improve your experience and our services by analyzing how you use our website. For more information, please read our{' '}
+                            <Link
+                                href="/policy"
+                                className="cursor-pointer underline hover:text-blue-500"
+                                target='_blank'
+                                rel={"noopener noreferrer"}
+                                aria-label="More info"
+                                aria-description='More info about cookies'
+                            >
+                                Cookies Policy
+                            </Link>
+                        </p>
+                        <button
+                            onClick={() => {
+                                handleConsent('accepted');
+                            }}
+                            className="text-md self-end inline-block rounded-lg px-4 py-1 text-right font-semibold text-dark-green-15 transition duration-500 hover:bg-dark-green-40"
+                        >
+                            Accept
+                        </button>
+                    </div>
                 </div>
             </div>
         );
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,12 +3,12 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
 
     return {
+        sitemap: 'https://shifaref.com/sitemap.xml',
+        host: 'https://shifaref.com/',
         rules: {
             userAgent: '*',
             allow: '/',
             disallow: '/admin/',
         },
-        sitemap: 'https://shifaref.com/sitemap.xml',
-        host: 'https://shifaref.com/',
     }
 }
