fe860ea045860d9fe38fe3e2aecd2633393c944e
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-04-24 15:59:21 +0300

    feat: add @vercel/analytics to project; integrate analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "@/styles/globals.css";
 import { Toaster } from "@/_components/ui/sonner"
+import { Analytics } from "@vercel/analytics/react"
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -111,6 +112,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
       >
         <Toaster richColors />
         {children}
+        <Analytics />
       </body>
     </html>
   );
