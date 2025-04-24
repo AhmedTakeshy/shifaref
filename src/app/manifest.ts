2be5b0c102ceb27a28cb536801a2e6824ddde53f
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-04-24 16:29:12 +0300

    feat: remove unused favicon entries from manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -25,21 +25,6 @@ export default function manifest(): MetadataRoute.Manifest {
                 sizes: '180x180',
                 type: 'image/png',
             },
-            {
-                src: '/favicons/favicon-16x16.png',
-                sizes: '16x16',
-                type: 'image/png',
-            },
-            {
-                src: '/favicons/favicon-32x32.png',
-                sizes: '32x32',
-                type: 'image/png',
-            },
-            {
-                src: '/favicon.ico',
-                sizes: '48x48',
-                type: 'image/x-icon',
-            },
         ],
     }
 }
